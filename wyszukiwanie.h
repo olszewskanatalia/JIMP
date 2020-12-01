28f2c0933b965481e8336cd8ab248bf027581362
Natalia Olszewska <natliaolszewska@gmail.com>
2020-12-02 00:14:26 +0100

    naglowek wyszukiwanie

--- wyszukiwanie.h
+++ wyszukiwanie.h
@@ -4,6 +4,8 @@
 #ifndef _WYSZUKIWANIE_H_
 #define _WYSZUKIWANIE_H_
 
-void wyszukaj();
+void dodaj (int **skoro, int index_slowa, int nr_linii);
+
+void wyszukaj(FILE *plik, int ile_slow, int **skorowidz, char **slowa);
 
 #endif
