7b2ca1a4c289c320ab1bc4c343bd65bb52a02494
Natalia Olszewska <natliaolszewska@gmail.com>
2020-12-01 19:26:56 +0100

    chyba wypisze o ile realloc dziala

--- skorowidz.c
+++ skorowidz.c
@@ -1,4 +1,26 @@
 /* tutaj jest funkcja wypisujaca skorowidz :333 */
 
-#include "skorowidz.h"
+
+void wypisz_skorowidz(int **skoro,int *slowa, int ile_slow)
+{
+	for(int i = 0; i < ile_slow)
+	{
+		if(skoro[i][0] == 0 )
+		{
+			printf("Slowo: %s , nie wystapilo w zadnej linii pliku.\n",slowa[i]);
+		}
+		else
+		{
+			printf("Slowo: %s , wystapilo w lini(ach): \n",slowo[i]);
+
+			for(int k = 1; k < skoro[i][0]; k++)
+			{
+				printf(" %d ",skoro[i][k]);
+			}
+
+			printf("\n");
+		}
+	}
+
+}
 
